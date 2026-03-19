0447aa1a2fd47be35b87496e1f8782fcef3e5739
Максим Попков <your@email.com>
2026-03-19 19:46:17 +0300

    lab1: ApiSource на реальном API

--- lab1/src/sources/api_source.py
+++ lab1/src/sources/api_source.py
@@ -1,23 +1,33 @@
 from typing import Iterable
 from src.task import Task
+import requests
 
 class ApiSource:
     """
     Источник задач.
-    Эмулирует получение задач из внешнего API.
+    Получает задачи из реального внешнего API.
     """
 
-    def __init__(self):
-        self._api_response = [
-            {'id': 1515, 'payload': {'from': 'api', 'type': 'test', 'priority': 'high'}},
-            {'id': 1516, 'payload': {'from': 'api', 'type': 'test', 'priority': 'mid'}},
-            {'id': 1517, 'payload': {'from': 'api', 'type': 'test', 'priority': 'low'}},
-        ]
+    def __init__(self, url: str):
+        """
+        Инициализация.
+        :param url: URL внешнего API
+        """
+        self.url = url
 
     def get_tasks(self) -> Iterable[Task]:
         """
-        Возвращает задачи из API-заглушки.
+        Получает данные из API и преобразует их в объекты Task.
         :return: Итерируемая коллекция объектов Task
         """
-        for t in self._api_response:
-            yield Task(t['id'], t['payload'])
+
+        resp = requests.get(self.url, timeout=5)
+        resp.raise_for_status()
+        api_tasks = resp.json()
+
+        for t in api_tasks:
+            yield Task(t['id'], {
+                "userId": t["userId"],
+                "title": t["title"],
+                "completed": t["completed"]
+            })
